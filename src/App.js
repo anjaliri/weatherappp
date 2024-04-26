076af7c9e3916bb9d85e2d8f02cf1d3571e9066f
anjaliri <anjalisikanya@gmail.com>
2024-04-26 18:29:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css'
+import WeaatherApp from './Components/WeatherApp/WeatherApp'
+
+const App = () => {
+  return (
+    <div className='App'>
+      
+      <WeaatherApp/>
+    </div>
+  )
+}
+
+export default App
